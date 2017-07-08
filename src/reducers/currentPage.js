d736342e3594dbab9bb9adb4c5a64d5c89a91c1f
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2017-07-08 15:49:05 -0600

    interview - create the currentPage reducer

---
+++ src/reducers/currentPage.js
@@ -0,0 +1,10 @@
+export default function currentPage(state = 1, action) {
+    switch(action.type) {
+        case 'CURRENT_PAGE/INCREMENT':
+            return state < 9 ? state + 1 : state;
+        case 'CURRENT_PAGE/DECREMENT':
+            return state > 1 ? state - 1 : state;
+        default:
+            return state;
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import people from './people';
 import planets from './planets';
+import currentPage from './currentPage';
 
 
 export default combineReducers({
     people,
     planets,
+    currentPage,
 });
