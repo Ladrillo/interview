f0ca1f7750b04b5c4b4b4464197dc4820e11c190
Gabriel Cabrejas Poole <gabriel@cabrejas.net>
2017-07-08 17:06:33 -0600

    interview - debounce input changes in the SearchBar

--- src/components/search_bar/SearchBar.js
+++ src/components/search_bar/SearchBar.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/merge';
 import './SearchBar.css';
+
 
 export default class SearchBar extends Component {
     constructor() {
         super();
-
         this.state = {
             searchTerm: ''
         };
         this.onChange = this.onChange.bind(this);
     }
 
+    componentDidMount() {
+        // we only want to AJAX  after 200 milliseconds of 'radio silence'
+        const input = document.querySelector('.search-bar-input');
+        const { searchHandler } = this.props;
+
+        const stream = Observable.fromEvent(input, 'input');
+
+        stream
+            .debounceTime(200)
+            .subscribe(e => searchHandler(this.state.searchTerm));
+    }
+
     onChange(event) {
         const searchTerm = event.target.value;
-
         this.setState({ searchTerm });
-        this.props.searchHandler(searchTerm);
     }
 
     render() {
         return (
             <div className='search-bar'>
                 <input
+                    className   = 'search-bar-input'
                     placeholder = 'Search Your Destiny'
                     value       = {this.state.searchTerm}
                     onChange    = {this.onChange}
